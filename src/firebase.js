68e9ea592ff01d065bf0185e777cad84e25fb9ee
gkaragoz <gkaragoz96@gmail.com>
2020-08-03 19:12:10 +0300

    Create video looking design with jsx and css.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKLRlM-5l95L1KCZ6VISgjGkuTfH2F0s0",
+  authDomain: "react-tiktok-clone.firebaseapp.com",
+  databaseURL: "https://react-tiktok-clone.firebaseio.com",
+  projectId: "react-tiktok-clone",
+  storageBucket: "react-tiktok-clone.appspot.com",
+  messagingSenderId: "268501270825",
+  appId: "1:268501270825:web:5455a36be9a9d2544bf738",
+  measurementId: "G-3DPS7WJQ70",
+};
